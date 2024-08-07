b66e818fe039e636308da8df9a9301fc0b255db4
Fernando Kuteken <fskuteken@gmail.com>
2024-08-07 17:30:12 -0300

    Add test for chats-client

---
+++ src/clients/chats/chats-client.test.ts
@@ -0,0 +1,29 @@
+import axios from 'axios'
+import MockAdapter from 'axios-mock-adapter'
+import { randomUUID } from 'crypto'
+import { describe, expect, test } from 'vitest'
+import { ListChatMessagesQuery, ListChatMessagesResult } from './types'
+import { Cogfy } from '../../cogfy'
+
+describe('chats', () => {
+  describe('listMessages', () => {
+    test('serializes roles', async () => {
+      const mock = new MockAdapter(axios)
+      const cogfy = new Cogfy()
+      const collectionId = randomUUID()
+      const chatId = randomUUID()
+      const params: ListChatMessagesQuery = {
+        roles: ['assistant', 'user']
+      }
+      mock
+        .onGet(`https://api.cogfy.com/collections/${collectionId}/chats/${chatId}/messages`, { params })
+        .replyOnce<ListChatMessagesResult>(200, { data: [], pageNumber: 0, pageSize: 10, totalSize: 0 })
+
+      const response = await cogfy.chats.listMessages(collectionId, chatId, params)
+
+      expect(response).toBeDefined()
+
+      mock.reset()
+    })
+  })
+})
