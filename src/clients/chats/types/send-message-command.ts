348283ac81f86f16213009ded35bea24e6d4eba2
Gabriel Scapim <scapimgabriel@gmail.com>
2025-02-19 18:28:59 -0300

    Update send message params

--- src/clients/chats/types/send-message-command.ts
+++ src/clients/chats/types/send-message-command.ts
@@ -1,3 +1,18 @@
+type Bytes = number
+type MimeType =
+  'audio/aac' |
+  'audio/mpeg' |
+  'audio/ogg' |
+  'audio/wav' |
+  'audio/webm'
+
 export type SendMessageCommand = {
   content: string
+  contentType?: 'text' | 'audio' | null
+  file?: {
+    name: string | null
+    size: Bytes | null
+    mimeType: MimeType
+    base64: string
+  } | null
 }
--- src/clients/chats/types/send-message-result.ts
+++ src/clients/chats/types/send-message-result.ts
@@ -1,4 +1,12 @@
+type Bytes = number
+
 export type SendMessageResult = {
   id: string
   content: string
+  files: {
+    name: string | null
+    size: Bytes
+    mimeType: string
+    base64: string
+  }[]
 }
